c893d8da6a47764cc974a16175430d7046dbbb86
Gaurao Mate <99711633+GauraoM@users.noreply.github.com>
2022-04-14 12:21:08 +0530

    Update send_mail.py

--- feedback app/send_mail.py
+++ feedback app/send_mail.py
@@ -4,13 +4,13 @@ from email.mime.text import MIMEText
 def send_mail(customer, dealer, rating, comments):
     port = 2525
     smtp_server = 'smtp.mailtrap.io'
-    login = '0bd2fabe458ec0'
-    password = '774f9e84eaaa9d'
+    login = '###########'
+    password = '##########'
     
     message = f"<h3>New Feedback Submission</h3><ul><li>Customer: {customer}</li><li>Dealer: {dealer}</li><li>Rating: {rating}</li><li>Comments: {comments}</li></ul>"
 
-    sender_email = 'gaurao.mate@konverge.ai'
-    receiver_email = 'gauravmate14@gmail.com'
+    sender_email = '###################'
+    receiver_email = '################'
     msg = MIMEText(message, 'html')
     msg['Subject'] = 'Lexus Feedback'
     msg['From'] = sender_email
